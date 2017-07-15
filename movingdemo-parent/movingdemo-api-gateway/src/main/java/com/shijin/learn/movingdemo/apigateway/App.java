494781597e88970f87d43c7e80155a3a4289f889
shijin <shijin@192.168.3.7>
2017-07-15 11:27:26 +0800

    make api-gateway protected by Oauth2Sso

--- movingdemo-parent/movingdemo-api-gateway/src/main/java/com/shijin/learn/movingdemo/apigateway/App.java
+++ movingdemo-parent/movingdemo-api-gateway/src/main/java/com/shijin/learn/movingdemo/apigateway/App.java
@@ -2,7 +2,12 @@ package com.shijin.learn.movingdemo.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * 
@@ -10,11 +15,21 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @EnableZuulProxy
 @SpringBootApplication
-public class App
-{
+@EnableOAuth2Sso
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class App extends WebSecurityConfigurerAdapter {
 
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class, args);
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.httpBasic().disable();
+    http.authorizeRequests()
+      .antMatchers("/uaa/**").permitAll()
+      .anyRequest().authenticated()
+      .and()
+      .csrf().disable();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
 }
